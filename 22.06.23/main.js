cace1fcd25000ae71f17b449eb0a9bc0e6f2b934
Iskander <135708220+haentis@users.noreply.github.com>
2023-07-16 17:42:29 +0600

    11

--- 22.06.23/main.js
+++ 22.06.23/main.js
@@ -23,3 +23,57 @@ fs.readFile('data.json', 'utf8', (err, data) => {
     }
   });
 });
+
+
+
+
+const fs = require('fs');
+const path = require('path');
+const convert = require('xml-js');
+
+const args = process.argv.slice(2);
+const sourceFile = args[0];
+const destinationFile = args[1];
+
+const sourceExtension = path.extname(sourceFile).toLowerCase();
+const destinationExtension = path.extname(destinationFile).toLowerCase();
+
+if (sourceExtension === '.json' && destinationExtension === '.xml') {
+  fs.readFile(sourceFile, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Ошибка при чтении файла:', err);
+      return;
+    }
+
+    const jsonData = JSON.parse(data);
+    const xmlData = convert.js2xml(jsonData, { compact: true, spaces: 2 });
+
+    fs.writeFile(destinationFile, xmlData, (err) => {
+      if (err) {
+        console.error('Ошибка при записи файла:', err);
+      } else {
+        console.log('Файл успешно конвертирован в XML:', destinationFile);
+      }
+    });
+  });
+} else if (sourceExtension === '.xml' && destinationExtension === '.json') {
+  fs.readFile(sourceFile, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Ошибка при чтении файла:', err);
+      return;
+    }
+
+    const xmlData = data;
+    const jsonData = convert.xml2js(xmlData, { compact: true, spaces: 2 });
+
+    fs.writeFile(destinationFile, JSON.stringify(jsonData), (err) => {
+      if (err) {
+        console.error('Ошибка при записи файла:', err);
+      } else {
+        console.log('Файл успешно конвертирован в JSON:', destinationFile);
+      }
+    });
+  });
+} else {
+  console.error('Неверное расширение файлов. Пожалуйста, укажите исходный файл в формате JSON и файл назначения в формате XML, или наоборот.');
+}
